20ab2b0adda4b7e8e4ee79e75b0a16f77961e934
Julio <juliokreds@gmail.com>
2025-10-26 16:52:55 -0300

    chore: refatorando logica de redirect no middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,48 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { getUserSession } from "./actions/get-user-session";
 
-const redirectUser = (route: string, userIsAtuthenticaded: boolean) => {
-  // Caso usuario autenticado, redireciona para a página principal
-  if (route === "/autenticar" && userIsAtuthenticaded) {
-    return "/";
-  }
+const VERIFY_ROUTE = [
+  {
+    path: "/autenticar",
+    requires_auth: false,
+    redirectTo: "/",
+  },
+  {
+    path: "/checkout",
+    requires_auth: true,
+    redirectTo: "/autenticar",
+  },
+  {
+    path: "/pedidos",
+    requires_auth: true,
+    redirectTo: "/autenticar",
+  },
+] as const;
 
-  // caso usuário não autenticado e quiser acessar pagina de checkout, redireciona para a página de autenticacao
-  if (route.startsWith("/checkout") && !userIsAtuthenticaded) {
-    return "/autenticar";
-  }
-};
 export async function middleware(request: NextRequest) {
   const userSession = await getUserSession();
   const pathName = request.nextUrl.pathname;
   const userIsAtuthenticaded = !!userSession;
-  const redirect = redirectUser(pathName, userIsAtuthenticaded);
 
-  if (redirect) {
-    return NextResponse.redirect(new URL(redirect, request.url));
+  const ROUTE_NEEDS_ACTION = VERIFY_ROUTE.find(
+    (route) => pathName === route.path
+  );
+
+  if (!ROUTE_NEEDS_ACTION) {
+    return NextResponse.next();
+  }
+  // Verifica a condicao da rota e se o usuario esta logado e redireciona
+  if (ROUTE_NEEDS_ACTION.requires_auth && !userIsAtuthenticaded) {
+    return NextResponse.redirect(
+      new URL(ROUTE_NEEDS_ACTION.redirectTo, request.url)
+    );
   }
 
-  return NextResponse.next();
+  if (!ROUTE_NEEDS_ACTION.requires_auth && userIsAtuthenticaded) {
+    return NextResponse.redirect(
+      new URL(ROUTE_NEEDS_ACTION.redirectTo, request.url)
+    );
+  }
 }
 
 export const config = {
