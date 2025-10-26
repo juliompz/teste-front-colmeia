a5ef0bfdbe1b16d4a1290b29286c872f899c3274
Julio <juliokreds@gmail.com>
2025-10-26 18:28:55 -0300

    chore:trocando iamgens logos

--- src/app/(system)/components/list-brands.tsx
+++ src/app/(system)/components/list-brands.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const ListBrands = () => {
   return (
     <div className="space-y-6">
-      <h3 className="px-5 font-semibold ">Navegue por marcas</h3>
+      <h3 className="px-5 font-semibold">Navegue por marcas</h3>
 
       <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
         {brands.map((brand) => (
@@ -16,7 +16,7 @@ const ListBrands = () => {
             <Image
               src={brand.logoUrl}
               alt={brand.name}
-              className="h-6 w-6"
+              className="h-8 w-8 dark:invert dark:brightness-0 dark:hue-rotate-180"
               width={100}
               height={100}
             />
